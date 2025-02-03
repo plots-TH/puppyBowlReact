cb8eb21ad2a92b9b06a35463151035ede22332b9
Tristan <119907414+plots-TH@users.noreply.github.com>
2025-02-03 15:40:42 -0500

    Updated file structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AllPlayers from "./AllPlayers";
+import SinglePlayer from "./SinglePlayer";
+import NavBar from "./NavBar";
+import NewPlayerForm from "./NewPlayerForm";
+
+function Main() {
+  return <div>Main</div>;
+}
+
+export default Main;
